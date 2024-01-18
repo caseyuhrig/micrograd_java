a83bc4a18eb4c1dfef0520411053284c26fc23cd
Casey Uhrig <casey@caseyuhrig.com>
2024-01-17 16:04:17 -0800

    Code cleanup

--- src/main/java/caseyuhrig/micrograd/Utils.java
+++ src/main/java/caseyuhrig/micrograd/Utils.java
@@ -9,16 +9,16 @@ public class Utils {
 
     public static ArrayList<ArrayList<Value>> of(final double[][] xs) {
         final var results = new ArrayList<ArrayList<Value>>();
-        for (var i = 0; i < xs.length; i++) {
-            results.add(of(xs[i]));
+        for (final double[] x : xs) {
+            results.add(of(x));
         }
         return results;
     }
 
     public static ArrayList<Value> of(final double[] xs) {
         final var result = new ArrayList<Value>();
-        for (var i = 0; i < xs.length; i++) {
-            result.add(new Value(xs[i]));
+        for (final double x : xs) {
+            result.add(new Value(x));
         }
         return result;
     }
@@ -27,15 +27,15 @@ public class Utils {
         return IntStream.concat(IntStream.of(valueToPrepend), Arrays.stream(originalArray)).toArray();
     }
 
-    private static double truncate(final double precision, double value) {
+    public static double truncate(final double precision, double value) {
         final double factor = Math.pow(10, precision);
         value = value * factor;
         value = Math.floor(value);
         return value / factor;
     }
 
-    public static String wrap(String text, int wrapLength) {
-        StringBuilder wrappedText = new StringBuilder();
+    public static String wrap(final String text, final int wrapLength) {
+        final StringBuilder wrappedText = new StringBuilder();
         int lastIndex = 0;
 
         while (lastIndex < text.length()) {
@@ -45,7 +45,7 @@ public class Utils {
                 break;
             }
 
-            int end = lastIndex + wrapLength;
+            final int end = lastIndex + wrapLength;
             int spaceIndex = text.lastIndexOf(' ', end);
 
             // If no space is found, force a break at wrapLength
@@ -53,46 +53,8 @@ public class Utils {
                 spaceIndex = end;
             }
 
-            wrappedText.append(text.substring(lastIndex, spaceIndex)).append("\n");
+            wrappedText.append(text, lastIndex, spaceIndex).append("\n");
             lastIndex = spaceIndex + 1; // Skip the space
-        }
-
-        return wrappedText.toString();
-    }
-
-    public static String wrapColon(String text, int wrapLength) {
-        StringBuilder wrappedText = new StringBuilder();
-        int lastIndex = 0;
-
-        while (lastIndex < text.length()) {
-            if (lastIndex + wrapLength >= text.length()) {
-                // Append the rest of the text if it's shorter than the wrap length
-                wrappedText.append(text.substring(lastIndex));
-                break;
-            }
-
-            int end = Math.min(lastIndex + wrapLength, text.length());
-            int colonIndex = text.indexOf(':', lastIndex);
-
-            if (colonIndex != -1 && colonIndex < end) {
-                // Find the first space before the colon
-                int spaceIndex = text.lastIndexOf(' ', colonIndex);
-                if (spaceIndex >= lastIndex) {
-                    end = spaceIndex; // Wrap at the space if it exists
-                } else {
-                    // No space found; find the next colon or wrap at the end of the wrap length
-                    int nextColonIndex = text.indexOf(':', colonIndex + 1);
-                    if (nextColonIndex != -1 && nextColonIndex < lastIndex + wrapLength) {
-                        end = nextColonIndex;
-                    } else {
-                        end = lastIndex + wrapLength;
-                    }
-                }
-            }
-
-            // Append the substring and a newline
-            wrappedText.append(text.substring(lastIndex, end)).append("\n");
-            lastIndex = end + 1; // Move to the character after the space or colon
         }
 
         return wrappedText.toString();
